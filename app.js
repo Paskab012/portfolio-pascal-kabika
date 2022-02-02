bf3c28c81de55cba5da9f44dcf28ddab6a6a0eae
Elerqsousy <mahmoud.eleresousy@gmail.com>
2022-02-02 03:08:07 +0200

    Add content dynamically demo

--- app.js
+++ app.js
@@ -17,3 +17,28 @@ menuLinks[1].addEventListener("click", mobileMenuT);
 const navLink = document.querySelectorAll(".navigate");
 
 navLink.forEach((e) => e.addEventListener("click", mobileMenuT));
+
+// object 
+
+const projectsData = {
+  project1 : {},
+}
+
+
+
+
+const projectsSection = document.getElementById('first-card-section');
+const card1 = document.createElement('div');
+card1.classList.add('all-components');
+const firstImage = document.createElement('div');
+firstImage.classList.add('first-image');
+card1.appendChild(firstImage);
+
+const card1Image = document.createElement('img');
+card1Image.setAttribute('src', './asset/nature.png');
+card1Image.setAttribute('alt', '');
+card1.appendChild(card1Image);
+const project1Heading = document.createElement('h1');
+project1Heading.innerText = "Tonic";
+firstImage.appendChild(project1Heading);
+projectsSection.appendChild(card1);
