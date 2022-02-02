54047188c26742ceb3ec1248243c5c4cd8615897
KABIKA681 <pascalkabikampan@gmail.com>
2022-02-02 11:42:19 +0200

    Correct linters errors

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-const selectAll = document.querySelector("body");
 const menuLinks = document.querySelectorAll(".menuTog");
 const mobileMenu = document.querySelector(".menu");
 
-console.log(menuLinks);
 // Display Mobile Menu
 
 function mobileMenuT() {
@@ -18,27 +16,24 @@ const navLink = document.querySelectorAll(".navigate");
 
 navLink.forEach((e) => e.addEventListener("click", mobileMenuT));
 
-// object 
+// object
 
 const projectsData = {
-  project1 : {},
-}
+  project1: {},
+};
 
-
-
-
-const projectsSection = document.getElementById('first-card-section');
-const card1 = document.createElement('div');
-card1.classList.add('all-components');
-const firstImage = document.createElement('div');
-firstImage.classList.add('first-image');
+const projectsSection = document.getElementById("first-card-section");
+const card1 = document.createElement("div");
+card1.classList.add("all-components");
+const firstImage = document.createElement("div");
+firstImage.classList.add("first-image");
 card1.appendChild(firstImage);
 
-const card1Image = document.createElement('img');
-card1Image.setAttribute('src', './asset/nature.png');
-card1Image.setAttribute('alt', '');
+const card1Image = document.createElement("img");
+card1Image.setAttribute("src", "./asset/nature.png");
+card1Image.setAttribute("alt", "");
 card1.appendChild(card1Image);
-const project1Heading = document.createElement('h1');
+const project1Heading = document.createElement("h1");
 project1Heading.innerText = "Tonic";
 firstImage.appendChild(project1Heading);
 projectsSection.appendChild(card1);
